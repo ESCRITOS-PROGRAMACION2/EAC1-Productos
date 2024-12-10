7dc946fd83d870fa576586d0b6c6ecb250cc79e2
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-12-10 15:30:48 -0300

    Update ProductoLimpieza.java

--- logica/ProductoLimpieza.java
+++ logica/ProductoLimpieza.java
@@ -27,6 +27,21 @@ public class ProductoLimpieza extends Producto{
 		return super.toString()+"\t" + nivel;
 	}
 	
+	//si el nivel 1 o 2 tendra un 20% de descuento sobre el precio sino 0
+	@Override
+	public double promocion() {
+		double promo1=super.promocion();
+		double promo2=0;
+		if (nivel==1|| nivel==2)
+			promo2=promo1*0.20;
+			promo1=promo1-promo2;
+		return promo1;
+			
+			
+		
+	}
+	
 	
 
 }
+
